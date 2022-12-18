a79046946bf4575786c06c279902649f2fcfa922
NataliaDrause <nataliadrause@gmail.com>
2022-12-17 20:30:26 -0500

    add setings options

--- filename-to-caption.php
+++ filename-to-caption.php
@@ -21,6 +21,9 @@
 
     // Add menu link to admin sidebar and settings page.
     add_action( 'admin_menu', array ( $this, 'admin_page' ) );
+
+    // Add database options for settings.
+    add_action( 'admin_init', array( $this, 'settings' ) );
     
   }
 
@@ -31,9 +34,39 @@
     add_options_page('Filename to Caption Settings', 'Filename to Caption', 'manage_options', 'filename-to-caption-settings', array( $this, 'settings_html'));
   }
 
-  function settings_html() {
-    echo "Hello World!";
+  function settings_html() { ?>
+    <div class="wrap">
+      <h1>Filename to Caption Settings</h1>
+      <p>Select an option.</p>
+      <form action="options.php" action="POST">
+        <?php 
+          settings_fields('filename_to_caption_plugin');
+          do_settings_sections('filename-to-caption-settings');
+          submit_button();
+        ?>
+      </form>
+    </div>
+  <?php }
+
+  // Add database options for settings.
+  function settings() {
+
+    // Add settings page section.
+    add_settings_section( 'fcp_first_section', null, null, 'filename-to-caption-settings' );
+
+    // Add Caption field.
+    add_settings_field( 'fcp_caption', 'Caption', array( $this, 'checkbox_html' ), 'filename-to-caption-settings', 'fcp_first_section', array( 'theName' => 'fcp_caption') );
+    register_setting('filename_to_caption_plugin', 'fcp_caption', array(
+      'sanitize_callback' => 'sanitize_text_field', 
+      'default' => '1',
+    ));
+
   }
+
+  // Checkbox HTML on Settings page.
+  function checkbox_html($args) { ?>
+    <input type="checkbox" name="<?php echo $args['theName']; ?>" value="1" <?php checked(get_option($args['theName']), '1') ?>>
+  <?php }
 
  }
 

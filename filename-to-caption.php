af7e23366e24e40ff00dc08aee75a8214f8cc3bf
NataliaDrause <nataliadrause@gmail.com>
2023-01-09 10:57:40 -0500

    bug fix and add comments to code

--- filename-to-caption.php
+++ filename-to-caption.php
@@ -19,26 +19,27 @@
  class FilenameToCaptionPlugin {
   function __construct() {
 
-    // Add menu link to admin sidebar and settings page.
+    // 1. Add menu link to admin sidebar and settings page.
     add_action( 'admin_menu', array ( $this, 'admin_page' ) );
 
-    // Add database options for settings.
+    // 2. Add database options for settings.
     add_action( 'admin_init', array( $this, 'settings' ) );
     
   }
 
   // METHODS
 
-  // Add menu link to admin sidebar and settings page.
+  // 1. Add menu link to admin sidebar and settings page.
   function admin_page() {
     add_options_page('Filename to Caption Settings', 'Filename to Caption', 'manage_options', 'filename-to-caption-settings', array( $this, 'settings_html'));
   }
 
+  // 1.2. Setting page HTML.
   function settings_html() { ?>
     <div class="wrap">
       <h1>Filename to Caption Settings</h1>
       <p>Select an option.</p>
-      <form action="options.php" action="POST">
+      <form action="options.php" method="POST">
         <?php 
           settings_fields('filename_to_caption_plugin');
           do_settings_sections('filename-to-caption-settings');
@@ -48,22 +49,23 @@
     </div>
   <?php }
 
-  // Add database options for settings.
+  // 2. Add database options for settings.
   function settings() {
 
-    // Add settings page section.
-    add_settings_section( 'fcp_first_section', null, null, 'filename-to-caption-settings' );
+    // 2.3. Add settings page section.
+    add_settings_section( 'fcp_first_section', 'Select what you want to add', null, 'filename-to-caption-settings' );
 
-    // Add Caption field.
+    // 2.2.2 Add Caption field: build html input field.
     add_settings_field( 'fcp_caption', 'Caption', array( $this, 'checkbox_html' ), 'filename-to-caption-settings', 'fcp_first_section', array( 'theName' => 'fcp_caption') );
+    // 2.2.1 Add Caption field: register DB setting.
     register_setting('filename_to_caption_plugin', 'fcp_caption', array(
       'sanitize_callback' => 'sanitize_text_field', 
-      'default' => '1',
+      'default' => '0',
     ));
 
   }
 
-  // Checkbox HTML on Settings page.
+  // 2.4. Caption checkbox HTML on Settings page.
   function checkbox_html($args) { ?>
     <input type="checkbox" name="<?php echo $args['theName']; ?>" value="1" <?php checked(get_option($args['theName']), '1') ?>>
   <?php }
